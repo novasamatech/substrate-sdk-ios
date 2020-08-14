0e8ed287f03f9c0d3ae6d03bf15df08f2a15359b
Russel <emkil.russel@gmail.com>
2020-08-14 10:55:29 +0300

    fix keypair derivation for ed25519

--- FearlessUtils/Classes/Crypto/EcdsaKeypairFactory.swift
+++ FearlessUtils/Classes/Crypto/EcdsaKeypairFactory.swift
@@ -10,26 +10,19 @@ public struct EcdsaKeypairFactory: KeypairFactoryProtocol {
 
     public func createKeypairFromSeed(_ seed: Data,
                                       chaincodeList: [Chaincode]) throws -> IRCryptoKeypairProtocol {
-        let privateKey = try SECPrivateKey(rawData: seed)
-        let keypair = try internalFactory.derive(fromPrivateKey: privateKey)
-
-        guard !chaincodeList.isEmpty else {
-            return keypair
-        }
-
         let scaleEncoder = ScaleEncoder()
         try Self.hdkdPrefix.encode(scaleEncoder: scaleEncoder)
         let prefix = scaleEncoder.encode()
 
-        return try chaincodeList.reduce(keypair) { (keypair, chaincode) in
+        let childSeed = try chaincodeList.reduce(seed) { (currentSeed, chaincode) in
             guard chaincode.type == .hard else {
                 throw KeypairFactoryError.unsupportedChaincodeType
             }
 
-            let childSeed = try (prefix + keypair.privateKey().rawData() + chaincode.data).blake2b32()
-
-            let childPrivateKey = try SECPrivateKey(rawData: childSeed)
-            return try internalFactory.derive(fromPrivateKey: childPrivateKey)
+            return try (prefix + currentSeed + chaincode.data).blake2b32()
         }
+
+        let childPrivateKey = try SECPrivateKey(rawData: childSeed)
+        return try internalFactory.derive(fromPrivateKey: childPrivateKey)
     }
 }
