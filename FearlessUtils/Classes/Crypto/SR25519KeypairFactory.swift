da8dc5b0b6942a09d85161f455463a426bed2380
Russel <emkil.russel@gmail.com>
2020-08-17 12:32:17 +0300

    add derivable secret protocols implementation

--- FearlessUtils/Classes/Crypto/SR25519KeypairFactory.swift
+++ FearlessUtils/Classes/Crypto/SR25519KeypairFactory.swift
@@ -1,7 +1,7 @@
 import Foundation
 import IrohaCrypto
 
-public struct SR25519KeypairFactory: KeypairFactoryProtocol {
+public struct SR25519KeypairFactory: DerivableKeypairFactoryProtocol {
     let internalFactory = SNKeyFactory()
 
     public init() {}
@@ -10,7 +10,18 @@ public struct SR25519KeypairFactory: KeypairFactoryProtocol {
                                       chaincodeList: [Chaincode]) throws -> IRCryptoKeypairProtocol {
         let masterKeypair = try internalFactory.createKeypair(fromSeed: seed)
 
-        let childKeypair = try chaincodeList.reduce(masterKeypair) { (keypair, chaincode) in
+        let parentKeypair = IRCryptoKeypair(publicKey: masterKeypair.publicKey(),
+                                            privateKey: masterKeypair.privateKey())
+        return try deriveChildKeypairFromParent(parentKeypair, chaincodeList: chaincodeList)
+    }
+
+    public func deriveChildKeypairFromParent(_ keypair: IRCryptoKeypairProtocol,
+                                             chaincodeList: [Chaincode]) throws -> IRCryptoKeypairProtocol {
+        let privateKey = try SNPrivateKey(rawData: keypair.privateKey().rawData())
+        let publicKey = try SNPublicKey(rawData: keypair.publicKey().rawData())
+        let snKeypair: SNKeypairProtocol = SNKeypair(privateKey: privateKey, publicKey: publicKey)
+
+        let childKeypair = try chaincodeList.reduce(snKeypair) { (keypair, chaincode) in
             switch chaincode.type {
             case .soft:
                 return try internalFactory.createKeypairSoft(keypair, chaincode: chaincode.data)
