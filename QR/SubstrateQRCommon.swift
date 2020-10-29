69091454d11d3814ffb63210b7dca40953e392ce
Russel <emkil.russel@gmail.com>
2020-10-29 16:46:50 +0300

    fix old files

--- QR/SubstrateQRCommon.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public protocol SubstrateQREncodable {
-    func encode(receiverInfo: ReceiveInfo) throws -> Data {
-}
-
-public protocol SubstrateQRDecodable {
-    
-}
--- QR/SubstrateQRDecoder.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SubstrateQRDecoder.swift
-//  FearlessUtils
-//
-//  Created by Ruslan Rezin on 28.10.2020.
-//
-
-import Foundation
