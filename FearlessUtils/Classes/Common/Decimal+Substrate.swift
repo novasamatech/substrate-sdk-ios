1b92a4d7b665857374d622484fbea7601d995c17
Russel <emkil.russel@gmail.com>
2021-03-09 14:20:56 +0300

    add precision when converting between bigint and decimal

--- FearlessUtils/Classes/Common/Decimal+Substrate.swift
+++ FearlessUtils/Classes/Common/Decimal+Substrate.swift
@@ -2,18 +2,32 @@ import Foundation
 import BigInt
 
 public extension Decimal {
-    static func fromSubstrateAmount(_ value: BigUInt) -> Decimal? {
+    static var decimalToBigUIntHandler: NSDecimalNumberHandler {
+        NSDecimalNumberHandler(roundingMode: .down,
+                               scale: 0,
+                               raiseOnExactness: false,
+                               raiseOnOverflow: false,
+                               raiseOnUnderflow: false,
+                               raiseOnDivideByZero: false)
+    }
+
+    static func fromSubstrateAmount(_ value: BigUInt, precision: Int16) -> Decimal? {
         let valueString = String(value)
 
         guard let decimalValue = Decimal(string: valueString) else {
             return nil
         }
 
-        return (decimalValue as NSDecimalNumber).multiplying(byPowerOf10: -12).decimalValue
+        return (decimalValue as NSDecimalNumber).multiplying(byPowerOf10: -precision).decimalValue
     }
 
-    func toSubstrateAmount() -> BigUInt? {
-        let valueString = (self as NSDecimalNumber).multiplying(byPowerOf10: 12).stringValue
+    func toSubstrateAmount(precision: Int16) -> BigUInt? {
+        let valueString = (self as NSDecimalNumber)
+            .multiplying(byPowerOf10: precision, withBehavior: Self.decimalToBigUIntHandler).stringValue
         return BigUInt(valueString)
+    }
+
+    static func fromSubstratePerbill(value: BigUInt) -> Decimal? {
+        fromSubstrateAmount(value, precision: 9)
     }
 }
