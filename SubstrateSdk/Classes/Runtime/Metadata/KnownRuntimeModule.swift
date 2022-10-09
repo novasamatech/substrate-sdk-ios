3e3b83255392b5e31533972882b8c190d3f066dd
Russel <emkil.russel@gmail.com>
2022-10-09 11:46:25 +0500

    add fallback to alternative batch name

--- SubstrateSdk/Classes/Runtime/Metadata/KnownRuntimeModule.swift
+++ SubstrateSdk/Classes/Runtime/Metadata/KnownRuntimeModule.swift
@@ -1,9 +1,10 @@
 import Foundation
 
 public struct KnowRuntimeModule {
-    public struct Utitlity {
+    public struct Utility {
         public static let name = "Utility"
         public static let batch = "batch"
         public static let batchAll = "batch_all"
+        public static let batchAtomic = "batch_atomic"
     }
 }
