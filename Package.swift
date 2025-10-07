8a48162ec234989f28fcb4a89eea96c362f561ce
Den Vasilev <>
2025-10-07 17:53:43 +0200

    Updates Operation-iOS dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/novasamatech/Crypto-iOS", exact: "0.3.0"),
-        .package(url: "https://github.com/novasamatech/Operation-iOS", exact: "2.2.0"),
+        // TODO: - Temp solution for Operation-iOS, to be updated before merge
+        .package(
+            url: "https://github.com/novasamatech/Operation-iOS",
+            revision: "b32d53abbac2f12462737dba4e516d135f9efb2d"
+        ),
         .package(url: "https://github.com/ashleymills/Reachability.swift", exact: "5.2.4"),
         .package(url: "https://github.com/novasamatech/Starscream.git", exact: "4.0.13"),
         .package(url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap", exact: "1.1.0"),
--- SubstrateSdk/Classes/Network/WebSocketEngine.swift
+++ SubstrateSdk/Classes/Network/WebSocketEngine.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Operation_iOS
 import Starscream
 
 public protocol WebSocketConnectionProtocol: WebSocketClient {
