b29d35d2e09f80c2a94260bec1627a6264a0b496
Russel <emkil.russel@gmail.com>
2025-10-22 22:11:37 +0200

    make orml account public

--- SubstrateSdk/Classes/Primitives/OrmlAccount.swift
+++ SubstrateSdk/Classes/Primitives/OrmlAccount.swift
@@ -2,9 +2,9 @@ import Foundation
 import BigInt
 
 public struct OrmlAccount: Codable, Equatable {
-    @StringCodable var free: BigUInt
-    @StringCodable var reserved: BigUInt
-    @StringCodable var frozen: BigUInt
+    @StringCodable public var free: BigUInt
+    @StringCodable public var reserved: BigUInt
+    @StringCodable public var frozen: BigUInt
 
     public var total: BigUInt { free + reserved }
 }
