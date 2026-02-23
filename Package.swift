a7c0339fba44e2446826ca1573722571653752d6
svojsu <svojsuu@gmail.com>
2026-02-23 13:17:01 +0200

    fix branch name

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/novasamatech/Crypto-iOS", exact: "0.4.1"),
-        .package(url: "https://github.com/novasamatech/Operation-iOS", branch: "history-tracking"),
+        .package(url: "https://github.com/novasamatech/Operation-iOS", branch: "feature/history-tracking"),
         .package(url: "https://github.com/ashleymills/Reachability.swift", exact: "5.2.4"),
         .package(url: "https://github.com/novasamatech/Starscream.git", exact: "4.0.13"),
         .package(url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap", exact: "1.1.0"),
